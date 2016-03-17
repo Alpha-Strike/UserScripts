cf90531ae0794a1d1ccf2d339ab169745d09c544
Alpha-Strike <Alpha-Strike@users.noreply.github.com>
2016-03-17 23:54:08 +0100

    Update CleanTwitter.user.js

--- CleanTwitter.user.js
+++ CleanTwitter.user.js
@@ -1,1 +1,17 @@
+// ==UserScript==
+// @name         CleanTwitter
+// @namespace    https://github.com/Alpha-Strike/UserScripts
+// @version      0.1
+// @description  Get rid of annoying twitter widgets
+// @author       AlphaStrike
+// @license      MIT
+// @include      *://*.twitter.com/*
+// @include      *://twitter.com/*
+// @downloadURL  https://raw.githubusercontent.com/Alpha-Strike/UserScripts/master/CleanTwitter.user.js
+// @updateURL    https://raw.githubusercontent.com/Alpha-Strike/UserScripts/master/CleanTwitter.user.js
+// @grant        none
+// ==/UserScript==
 
+document.querySelector(".trends").style.display = "none";
+document.querySelector(".wtf-module").style.display = "none";
+document.querySelector(".Footer").style.display = "none";
